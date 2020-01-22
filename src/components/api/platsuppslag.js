ea63059e61c2496f88a69587d97b49120582c3aa
carlhernek <53989735+carlhernek@users.noreply.github.com>
2020-01-22 15:37:39 +0100

    Update platsuppslag.js

--- src/components/api/platsuppslag.js
+++ src/components/api/platsuppslag.js
@@ -1,17 +1,46 @@
 import React from "react";
 
-const ApiFetch = props => {
-  const key = "91055ad9865640ba844d8e2569f667ad";
-  const station = "true";
-  const maxResults = "1";
-  const proxy = "https://cors-anywhere.herokuapp.com/";
+class ApiFetch extends React.Component {
+  state = { locationData: {}, Name: "", SiteId: "", X: "", Y: "" };
+  locationData = async props => {
+    const key = "91055ad9865640ba844d8e2569f667ad";
+    const station = "true";
+    const maxResults = "1";
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const fake = "slussen";
 
-  fetch(
-    `${proxy}https://api.sl.se/api2/typeahead.json?key=${key}&searchstring=${props}&stationsonly=${station}&maxresults=${maxResults}`
-  ).then(res => res.json());
+    const api_call = await fetch(
+      `${proxy}https://api.sl.se/api2/typeahead.json?key=${key}&searchstring=${fake}&stationsonly=${station}&maxresults=${maxResults}`
+    );
 
-  console.log(ApiFetch);
-  return <div></div>;
-};
+    const data = await api_call.json();
+    const { ResponseData } = data;
+    ResponseData &&
+      ResponseData.map(item => {
+        this.setState({
+          Name: item.Name,
+          SiteId: item.SiteId,
+          X: item.X,
+          Y: item.Y
+        });
+      });
+    this.setState({ locationData: data });
+  };
+
+  componentDidMount() {
+    this.locationData();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.Name}</h1>
+        <h1>{this.state.SiteId}</h1>
+        <h1>{this.state.X}</h1>
+        <h1>{this.state.Y}</h1>
+      </div>
+    );
+  }
+}
 
 export default ApiFetch;
