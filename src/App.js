72df5b4484931993331430a4c2fb082cb574e681
anderskillander <55491817+anderskillander@users.noreply.github.com>
2020-01-23 12:33:45 +0100

    workshop 23/1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import ApiFetch from "./components/api/platsuppslag";
+import RealTidsInfo from "./components/api/realtidsinformation4";
 
 function App() {
   return (
     <div className="App">
       <ApiFetch />
+      <RealTidsInfo />
     </div>
   );
 }
--- src/components/api/platsuppslag.js
+++ src/components/api/platsuppslag.js
@@ -8,9 +8,9 @@ class ApiFetch extends React.Component {
     const maxResults = "1";
     const proxy = "https://cors-anywhere.herokuapp.com/";
     const fake = "slussen";
-
+    const format = "json"
     const api_call = await fetch(
-      `${proxy}https://api.sl.se/api2/typeahead.json?key=${key}&searchstring=${fake}&stationsonly=${station}&maxresults=${maxResults}`
+      `${proxy}https://api.sl.se/api2/typeahead.${format}?key=${key}&searchstring=${fake}&stationsonly=${station}&maxresults=${maxResults}`
     );
 
     const data = await api_call.json();
@@ -23,6 +23,7 @@ class ApiFetch extends React.Component {
           X: item.X,
           Y: item.Y
         });
+        // console.log(data);
         return item;
       });
     this.setState({ locationData: data });
@@ -35,10 +36,10 @@ class ApiFetch extends React.Component {
   render() {
     return (
       <div>
-        <h1>{this.state.Name}</h1>
-        <h1>{this.state.SiteId}</h1>
-        <h1>{this.state.X}</h1>
-        <h1>{this.state.Y}</h1>
+        <h1>Plats: <br />{this.state.Name}</h1>
+        <h1>SiteID: <br />{this.state.SiteId}</h1>
+        <h1>Long: <br />{this.state.X}</h1>
+        <h1>Lat: <br />{this.state.Y}</h1>
       </div>
     );
   }
